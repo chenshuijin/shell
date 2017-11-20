f92ea9146d805480ddf292f1216e1fd0def305ff
chenshuijin <785795635@qq.com>
2017-11-20 14:52:49 +0800

    add rename git commit user msg shell

---
+++ rename-git-user.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+rm -rf .git/refs/original/
+git filter-branch --env-filter '
+
+OLD_EMAIL="old-email@example.com"
+CORRECT_NAME="correct-name"
+CORRECT_EMAIL="correct-email@example.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
