2db888000c014150f597ffdd66d735a0312f2736
chenshuijin <785795635@qq.com>
2018-04-13 11:18:33 +0800

    add jdk mvn config

---
+++ config-jdk-mvn.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/usr/lib/jdk1.8.0_131
+export JRE_HOME=$JAVA_HOME/jre
+export CLASSPATH=.:$JAVA_HOME/lib:$JRE_HOME/lib
+export PATH=$PATH:$JAVA_HOME/bin:$JRE_HOME/bin:/usr/lib/apache-maven-3.5.0/bin
