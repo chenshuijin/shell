ad7bcb31458b6b664a2bc463a92937c05e63e392
chenshuijin <785795635@qq.com>
2018-04-13 11:17:40 +0800

    update tmux alias

--- config-tmux.sh
+++ config-tmux.sh
@@ -1,1 +1,14 @@
 echo 'set -g mode-mouse on' >> ~/.tmux.conf
+
+#alias tmux0='tmux attach-session -t 0'
+#alias tmux1='tmux attach-session -t 1'
+#alias tmux2='tmux attach-session -t 2'
+#alias tmux3='tmux attach-session -t 3'
+#alias tmux4='tmux attach-session -t 4'
+#alias tmux5='tmux attach-session -t 5'
+#alias tmux6='tmux attach-session -t 6'
+#alias tmux7='tmux attach-session -t 7'
+#alias tmux8='tmux attach-session -t 8'
+#alias tmux9='tmux attach-session -t 9'
+
+#alias tmuxl='tmux list-sessions'
