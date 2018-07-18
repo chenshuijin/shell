babda2160ff58dc3ea989f188f00efe414f148bb
chenshuijin <785795635@qq.com>
2018-07-18 14:45:18 +0800

    add reload firewall

--- openfirewallport.sh
+++ openfirewallport.sh
@@ -1,3 +1,4 @@
 firewall-cmd --zone=public --add-port=3001/tcp
 firewall-cmd --zone=public --remove-port=27017/tcp --permanent
 firewall-cmd --zone=public --add-port=27017/tcp --permanent
+firewall-cmd --reload
